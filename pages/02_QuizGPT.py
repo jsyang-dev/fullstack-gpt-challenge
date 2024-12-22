ae7c49d6bab1d80fff270f3d5e9d65a6a31b9246
Jeongsu Yang <mycat83@gmail.com>
2024-12-23 00:42:11 +0900

    fix: 에러 수정

--- pages/02_QuizGPT.py
+++ pages/02_QuizGPT.py
@@ -33,15 +33,6 @@ with st.sidebar:
     level = st.selectbox("Choose level of difficulty", ("Easy", "Medium", "Hard"))
 
 
-llm = ChatOpenAI(
-    temperature=0.1,
-    model="gpt-3.5-turbo-1106",
-    streaming=True,
-    callbacks=[StreamingStdOutCallbackHandler()],
-    api_key=openai_api_key,
-)
-
-
 def format_docs(docs):
     return "\n\n".join(document.page_content for document in docs)
 
@@ -50,174 +41,184 @@ def get_level(_):
     return level
 
 
-questions_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-            You are a helpful assistant that is role playing as a teacher.
-                
-            Based ONLY on the following context make 10 (TEN) questions minimum to test the user's knowledge about the text.
+if openai_api_key:
+    llm = ChatOpenAI(
+        temperature=0.1,
+        model="gpt-3.5-turbo-1106",
+        streaming=True,
+        callbacks=[StreamingStdOutCallbackHandler()],
+        api_key=openai_api_key,
+    )
 
-            Questions are created at selected level. It is possible to have 3 levels: Easy, Medium and Hard.
-            
-            Each question should have 4 answers, three of them must be incorrect and one should be correct.
-                
-            Use (o) to signal the correct answer.
-                
-            Question examples:
-                
-            Question: What is the color of the ocean?
-            Answers: Red|Yellow|Green|Blue(o)
-                
-            Question: What is the capital or Georgia?
-            Answers: Baku|Tbilisi(o)|Manila|Beirut
-                
-            Question: When was Avatar released?
-            Answers: 2007|2001|2009(o)|1998
-                
-            Question: Who was Julius Caesar?
-            Answers: A Roman Emperor(o)|Painter|Actor|Model
-                
-            Your turn!
-            
-            Level: {level}
+    questions_prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """
+                You are a helpful assistant that is role playing as a teacher.
+                    
+                Based ONLY on the following context make 10 (TEN) questions minimum to test the user's knowledge about the text.
 
-            Context: {context}
+                Questions are created at selected level. It is possible to have 3 levels: Easy, Medium and Hard.
+                
+                Each question should have 4 answers, three of them must be incorrect and one should be correct.
+                    
+                Use (o) to signal the correct answer.
+                    
+                Question examples:
+                    
+                Question: What is the color of the ocean?
+                Answers: Red|Yellow|Green|Blue(o)
+                    
+                Question: What is the capital or Georgia?
+                Answers: Baku|Tbilisi(o)|Manila|Beirut
+                    
+                Question: When was Avatar released?
+                Answers: 2007|2001|2009(o)|1998
+                    
+                Question: Who was Julius Caesar?
+                Answers: A Roman Emperor(o)|Painter|Actor|Model
+                    
+                Your turn!
+                
+                Level: {level}
+
+                Context: {context}
+                """,
+            )
+        ]
+    )
+
+    questions_chain = (
+        {"context": format_docs, "level": get_level} | questions_prompt | llm
+    )
+
+    formatting_prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """
+                You are a powerful formatting algorithm.
+                
+                You format exam questions into JSON format.
+                Answers with (o) are the correct ones.
+                
+                Example Input:
+
+                Question: What is the color of the ocean?
+                Answers: Red|Yellow|Green|Blue(o)
+                    
+                Question: What is the capital or Georgia?
+                Answers: Baku|Tbilisi(o)|Manila|Beirut
+                    
+                Question: When was Avatar released?
+                Answers: 2007|2001|2009(o)|1998
+                    
+                Question: Who was Julius Caesar?
+                Answers: A Roman Emperor(o)|Painter|Actor|Model
+                
+                
+                Example Output:
+                
+                ```json
+                {{ "questions": [
+                        {{
+                            "question": "What is the color of the ocean?",
+                            "answers": [
+                                    {{
+                                        "answer": "Red",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "Yellow",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "Green",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "Blue",
+                                        "correct": true
+                                    }},
+                            ]
+                        }},
+                                    {{
+                            "question": "What is the capital or Georgia?",
+                            "answers": [
+                                    {{
+                                        "answer": "Baku",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "Tbilisi",
+                                        "correct": true
+                                    }},
+                                    {{
+                                        "answer": "Manila",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "Beirut",
+                                        "correct": false
+                                    }},
+                            ]
+                        }},
+                                    {{
+                            "question": "When was Avatar released?",
+                            "answers": [
+                                    {{
+                                        "answer": "2007",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "2001",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "2009",
+                                        "correct": true
+                                    }},
+                                    {{
+                                        "answer": "1998",
+                                        "correct": false
+                                    }},
+                            ]
+                        }},
+                        {{
+                            "question": "Who was Julius Caesar?",
+                            "answers": [
+                                    {{
+                                        "answer": "A Roman Emperor",
+                                        "correct": true
+                                    }},
+                                    {{
+                                        "answer": "Painter",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "Actor",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "Model",
+                                        "correct": false
+                                    }},
+                            ]
+                        }}
+                    ]
+                }}
+                ```
+                Your turn!
+
+                Questions: {context}
+
             """,
-        )
-    ]
-)
+            )
+        ]
+    )
 
-
-questions_chain = {"context": format_docs, "level": get_level} | questions_prompt | llm
-
-formatting_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-            You are a powerful formatting algorithm.
-            
-            You format exam questions into JSON format.
-            Answers with (o) are the correct ones.
-            
-            Example Input:
-
-            Question: What is the color of the ocean?
-            Answers: Red|Yellow|Green|Blue(o)
-                
-            Question: What is the capital or Georgia?
-            Answers: Baku|Tbilisi(o)|Manila|Beirut
-                
-            Question: When was Avatar released?
-            Answers: 2007|2001|2009(o)|1998
-                
-            Question: Who was Julius Caesar?
-            Answers: A Roman Emperor(o)|Painter|Actor|Model
-            
-            
-            Example Output:
-            
-            ```json
-            {{ "questions": [
-                    {{
-                        "question": "What is the color of the ocean?",
-                        "answers": [
-                                {{
-                                    "answer": "Red",
-                                    "correct": false
-                                }},
-                                {{
-                                    "answer": "Yellow",
-                                    "correct": false
-                                }},
-                                {{
-                                    "answer": "Green",
-                                    "correct": false
-                                }},
-                                {{
-                                    "answer": "Blue",
-                                    "correct": true
-                                }},
-                        ]
-                    }},
-                                {{
-                        "question": "What is the capital or Georgia?",
-                        "answers": [
-                                {{
-                                    "answer": "Baku",
-                                    "correct": false
-                                }},
-                                {{
-                                    "answer": "Tbilisi",
-                                    "correct": true
-                                }},
-                                {{
-                                    "answer": "Manila",
-                                    "correct": false
-                                }},
-                                {{
-                                    "answer": "Beirut",
-                                    "correct": false
-                                }},
-                        ]
-                    }},
-                                {{
-                        "question": "When was Avatar released?",
-                        "answers": [
-                                {{
-                                    "answer": "2007",
-                                    "correct": false
-                                }},
-                                {{
-                                    "answer": "2001",
-                                    "correct": false
-                                }},
-                                {{
-                                    "answer": "2009",
-                                    "correct": true
-                                }},
-                                {{
-                                    "answer": "1998",
-                                    "correct": false
-                                }},
-                        ]
-                    }},
-                    {{
-                        "question": "Who was Julius Caesar?",
-                        "answers": [
-                                {{
-                                    "answer": "A Roman Emperor",
-                                    "correct": true
-                                }},
-                                {{
-                                    "answer": "Painter",
-                                    "correct": false
-                                }},
-                                {{
-                                    "answer": "Actor",
-                                    "correct": false
-                                }},
-                                {{
-                                    "answer": "Model",
-                                    "correct": false
-                                }},
-                        ]
-                    }}
-                ]
-            }}
-            ```
-            Your turn!
-
-            Questions: {context}
-
-        """,
-        )
-    ]
-)
-
-formatting_chain = formatting_prompt | llm
+    formatting_chain = formatting_prompt | llm
 
 
 @st.cache_data(show_spinner="Loading file...")
